2fe42c6447f874cd92db86400a163c9ab1fd0d48
ashna <amehta0082@conestogac.on.ca>
2020-02-19 18:49:02 -0500

    TriangleSolver working

---
+++ Prog8080_Asssignment2/TriangleSolver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog8080_Asssignment2
+{
+    class TriangleSolver
+    {
+        public static string Analyze(int firstDimension, int secondDimension, int thridDimension)
+        {
+            
+            string opString = "These dimensions could not form a triangle";
+            string triangleName;
+            if (CheckValidityOfDimensions(firstDimension, secondDimension, thridDimension))
+            {
+                Console.WriteLine("\n These dimensions can form a triangle");
+                triangleName = CheckTriangle(firstDimension, secondDimension, thridDimension);
+                opString = triangleName;
+                
+            }
+            else
+            {
+                //Console.WriteLine(opString);
+            }
+            return opString;
+        }
+        public bool IsTriangleValid(int sideOne, int sideTwo, int sideThree)
+        {
+            if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo || sideTwo + sideThree <= sideOne)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public string CheckTriagnleType(int sideOne, int sideTwo, int sideThree)
+        {
+            string triangleType = "nothing defined";
+            if (sideOne == sideTwo && sideTwo == sideThree)
+            {
+                triangleType = "Equilateral triangle";
+            }
+            else if (sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree)
+            {
+                triangleType = "Isosceles triangle";
+            }
+            else
+            {
+                triangleType = "Scalene triangle";
+            }
+            return triangleType;
+        }
+    
+}
+}
