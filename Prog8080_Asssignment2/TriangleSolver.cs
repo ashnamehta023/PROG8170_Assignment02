e46c5418755f62b1a77ced6790857a9ed115709e
ashna <amehta0082@conestogac.on.ca>
2020-02-19 20:19:32 -0500

    program class working

--- Prog8080_Asssignment2/TriangleSolver.cs
+++ Prog8080_Asssignment2/TriangleSolver.cs
@@ -10,45 +10,43 @@ namespace Prog8080_Asssignment2
     {
         public static string Analyze(int sideOne, int sideTwo, int sideThree)
         {
-            string result = "";
-            string traingleType;
-            if (IsTriangleSidesValid(sideOne, sideTwo, sideThree)==0)
-            {
-                Console.WriteLine("\n These dimensions can form a triangle");
-                traingleType = CheckTriagnleType(sideOne, sideTwo, sideThree);
-                result = traingleType;
-            }
-            else
-            {
-                result = "These dimensions could not form a triangle";
-            }
+            string result = CheckTriagnleType(sideOne, sideTwo, sideThree);
             return result;
         }
-        public static int IsTriangleSidesValid(int sideOne, int sideTwo, int sideThree)
-        {
-            if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo || sideTwo + sideThree <= sideOne)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+
         public static string CheckTriagnleType(int sideOne, int sideTwo, int sideThree)
         {
             string triangleType = "nothing defined";
-            if (sideOne == sideTwo && sideTwo == sideThree)
+            if(sideOne == 0 || sideTwo == 0 || sideThree == 0)
             {
+                triangleType = "No side of a triangle can be Zero\n----------------------\n";
+            }
+            else if (sideOne == sideTwo && sideTwo == sideThree)
+            {
+                Console.WriteLine("\n These dimensions can form a triangle\n");
                 triangleType = "Equilateral triangle";
             }
             else if (sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree)
             {
-                triangleType = "Isosceles triangle";
+                if (sideOne + sideTwo >= sideThree && sideOne + sideThree >= sideTwo && sideTwo + sideThree >= sideOne)
+                {
+                    triangleType = "These dimensions can not form a triangle\n----------------------\n";
+                }
+                else
+                {
+                    triangleType = "These dimensions can form a triangle.\n Isosceles triangle\n----------------------\n";
+                }
             }
-            else if(sideOne != sideTwo || sideOne != sideThree || sideTwo != sideThree)
+            else if (sideOne != sideTwo || sideOne != sideThree || sideTwo != sideThree)
             {
-                triangleType = "Scalene triangle";
+                if (sideOne + sideTwo >= sideThree && sideOne + sideThree >= sideTwo && sideTwo + sideThree >= sideOne)
+                {
+                    triangleType = "These dimensions can not form a triangle\n----------------------\n";
+                }
+                else
+                {
+                    triangleType = "These dimensions can form a triangle.\nScalene triangle\n----------------------\n";
+                }
             }
             return triangleType;
         }
